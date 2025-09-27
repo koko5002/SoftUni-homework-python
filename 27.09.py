5802da518b10448ea72ec99b1027b7f1ce1c7d87
Kaloyan Kotlarov <kaloyankotlarov@yahoo.com>
2025-09-28 00:33:06 +0300

    homework from 17.09.25

---
+++ 27.09.py
@@ -0,0 +1,74 @@
+"""#zad1
+inp = input()
+splitList = inp.split(" ")
+aim= []
+for i in range(len(splitList)):
+    a = int(splitList[i])*(-1)
+    aim.append(a)
+print(aim)
+
+#zad2
+factor= int(input())
+count = int(input())
+list1=[]
+for i in range(1,count+1):
+    a=i*factor
+    list1.append(a)
+print(list1)
+
+#zad3
+a=11
+b=11
+foundNumsA = []
+foundNumsB = []
+cards = input()
+list1=cards.split(" ")
+gameOver = False
+for i in range(len(list1)):
+    temp = list1[i].split("-")
+
+    if ("A" in temp[0]) and (temp[1] not in foundNumsA):
+        a-=1
+        foundNumsA.append(temp[1])
+    elif ("B" in temp[0]) and (temp[1] not in foundNumsB):
+        b-=1
+        foundNumsB.append(temp[1])
+    else:
+        continue
+    if a < 7 or b < 7:
+        gameOver=True
+        break
+print(f"Team A - {a}; Team B - {b}")
+if gameOver:
+    print("Game was terminated")
+
+#zad4
+numbers = list(map(int, input().split(", ")))
+beggars_count = int(input())
+
+result = []
+
+for beggar_index in range(beggars_count):
+    total = 0
+    for i in range(beggar_index, len(numbers), beggars_count):
+        total += numbers[i]
+    result.append(total)
+
+print(result)
+"""
+#zad5
+inp = input()
+list1 = [int(x) for x in inp.split(" ")]
+n=int(input())
+# copy and sort to find the n smallest
+temp = list1.copy()
+temp.sort()
+# remove the n smallest
+smallest_to_remove = temp[:n]
+# build result: keep only elements not in smallest_to_remove
+result = list1.copy()
+for num in smallest_to_remove:
+    result.remove(num)  # removes only one occurrence each
+
+# print
+print(", ".join(str(x) for x in result))
